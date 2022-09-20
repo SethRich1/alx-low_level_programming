860befc74ca745769fd1a5a8f04e8c41f86a03ec
SethRich1 <111015895+SethRich1@users.noreply.github.com>
2022-09-20 01:14:33 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,16 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+  * _puts - prints the string on the stdout
+  * @str: string to be printed
+  */
+void _puts(char *str)
 {
-	return (write(1, &c, 1));
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
